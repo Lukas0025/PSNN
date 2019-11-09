0dd393d665a45e15bab2ce1902a16698bacca2c9
Lukáš Plevač <lukasplevac@gmail.com>
2019-11-09 22:37:34 +0100

    Set version to 0.0.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PYSNN",
-    version="0.0.8",
+    version="0.0.9",
     author="Lukáš Plevač",
     author_email="lukasplevac@gmail.com",
     description="PYthon Simple Neural Network - PYSNN is python3 lib for machine learning",
